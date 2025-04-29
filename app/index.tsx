536da9dc78a3a9970fa9cdedbeec4dbf7ee874ba
lara gonçalves <lara.cecilia@academico.ifpb.edu.br>
2025-04-29 11:33:05 -0300

    lista de usuarios

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
 
+const usuarios = [
+  {
+    id: 1,
+    nome: "Lara",
+    cargo: "Aluna"
+},
+  {
+    id: 2,
+    nome: "Darlla",
+    cargo: "Aluna" 
+}
+];
+
 export default function Index() {
   return (
     <View
      style={style.container}
     >
-      <Text>olá</Text>
+      {
+        usuarios.map(item => (<Text style= {{ fontSize: 20}}>{item.nome}</Text>))
+      }
     </View>
   );
 }
